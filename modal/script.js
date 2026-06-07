4214404b854ad0c0cb26a8183803d1b5bd500b3f
AdnaanBashir01 <sheikhadnan87761@gmail.com>
2026-06-07 13:23:29 +0530

    handling keypress

--- modal/script.js
+++ modal/script.js
@@ -27,3 +27,9 @@ overlay.addEventListener("click", closeModal);
 //   modal.classList.add("hidden");
 //   overlay.classList.add("hidden");
 // });
+document.addEventListener("keydown", function (e) {
+  // console.log(e.key);
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
