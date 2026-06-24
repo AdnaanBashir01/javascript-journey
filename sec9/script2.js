975dba45ab739dfb259a149c72c8dbebb3082a24
AdnaanBashir01 <sheikhadnan87761@gmail.com>
2026-06-24 21:07:04 +0530

    object literals

--- sec9/script2.js
+++ sec9/script2.js
@@ -1,4 +1,22 @@
 "use strict";
+
+const weekdays = ["mon", "tue", "wed", "thr", "fri", "sat", "sun"];
+
+const openingHours = {
+  [weekdays[3]]: {
+    open: 12,
+    close: 22,
+  },
+  [weekdays[4]]: {
+    open: 11,
+    close: 23,
+  },
+  [weekdays[5]]: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+
 const restaurant = {
   name: "Classico Italiano",
   location: "Via Angelo Tavanti 23, Firenze, Italy",
@@ -6,40 +24,25 @@ const restaurant = {
   starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
   mainMenu: ["Pizza", "Pasta", "Risotto"],
 
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
-  order: function (starterIndex, mainIndex) {
+  //ES6 enhanced object literals
+  openingHours,
+  //  order: function (starterIndex, mainIndex) {
+  //     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  order(starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
 
-  orderDelivery: function ({
-    starterIndex = 1,
-    mainIndex = 0,
-    time = "20,00",
-    address,
-  }) {
+  orderDelivery({ starterIndex = 1, mainIndex = 0, time = "20,00", address }) {
     console.log(
       `Order recieved! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delieverd to ${address} at ${time}`,
     );
   },
 
-  orderPasta: function (ing1, ing2, ing3) {
+  orderPasta(ing1, ing2, ing3) {
     console.log(`Here is your delicious pasta with ${ing1} ,${ing2}, ${ing3} `);
   },
 
-  orderPizza: function (mainIngredeint, ...otherIngredeint) {
+  orderPizza(mainIngredeint, ...otherIngredeint) {
     console.log(mainIngredeint);
     console.log(otherIngredeint);
   },
@@ -55,13 +58,13 @@ const restaurant = {
 //   owner: "rosssi",
 // };
 
-const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+// const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
 
-for (const item of menu) console.log(item);
+// for (const item of menu) console.log(item);
 
-for (const [i, el] of menu.entries()) {
-  console.log(`${i + 1}: ${el}`);
-}
+// for (const [i, el] of menu.entries()) {
+//   console.log(`${i + 1}: ${el}`);
+// }
 
 // OR assingment
 // rest1.numGuests = rest1.numGuests || 10;
