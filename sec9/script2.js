fdca89120130a81d355c907d430717d9e369bd6a
AdnaanBashir01 <sheikhadnan87761@gmail.com>
2026-06-27 23:20:09 +0530

    optimal chaining

--- sec9/script2.js
+++ sec9/script2.js
@@ -48,6 +48,32 @@ const restaurant = {
   },
 };
 
+if (restaurant.openingHours && restaurant.openingHours.mon)
+  console.log(restaurant.openingHours.mon.open);
+
+//optional chaining
+console.log(restaurant.openingHours.mon?.open);
+console.log(restaurant.openingHours?.mon?.open);
+
+const days = ["mon", "tue", "wed", "thr", "fri", "sat", "sun"];
+for (const day of days) {
+  //day getting elements dyanamically
+  console.log(day);
+  const open = restaurant.openingHours[day]?.open ?? "closed";
+  console.log(`On ${day}, we open at ${open}`);
+}
+
+//methods
+console.log(restaurant.order?.(0, 1) ?? "method doesnt exist");
+console.log(restaurant.orderRisotto?.(0, 1) ?? "method doesnt exist");
+
+//arrays
+const users = [{ name: "adnan", email: "adnnan@gmail" }];
+console.log(users[0]?.name ?? "user array empty"); //?. value on the left exist
+if (users.length > 0)
+  console.log(users[0].name); //withou optimal chaining
+else console.log("user array empty");
+
 // const rest1 = {
 //   name: "La italianp",
 //   numGuests: 0,
