5ccd8ae5527453a4adf6c09aba4c93ce120b7e1b
AdnaanBashir01 <sheikhadnan87761@gmail.com>
2026-07-04 21:20:38 +0530

    chall 3

--- sec9/script2.js
+++ sec9/script2.js
@@ -48,35 +48,76 @@ const restaurant = {
   },
 };
 
-//Maps
-const question = new Map([
-  ["question", "what is the best prog lang in the world"],
-  [1, "C"],
-  [2, "Java"],
-  [3, "JavaScript"],
-  ["correct", 3],
-  [true, "correct"],
-  [false, "try again"],
+const gameEvents = new Map([
+  [17, "⚽️ GOAL"],
+  [36, "🔁 Substitution"],
+  [47, "⚽️ GOAL"],
+  [61, "🔁 Substitution"],
+  [64, "🔶 Yellow card"],
+  [69, "🔴 Red card"],
+  [70, "🔁 Substitution"],
+  [72, "🔁 Substitution"],
+  [76, "⚽️ GOAL"],
+  [80, "⚽️ GOAL"],
+  [92, "🔶 Yellow card"],
 ]);
-console.log(question);
 
-// object to map
-console.log(Object.entries(openingHours));
-const hoursMap = new Map(Object.entries(openingHours));
-console.log(hoursMap);
+// 1.
+const events = [...new Set(gameEvents.values())];
+console.log(events);
 
-//iterations
-for (const [keys, values] of question) {
-  if (typeof key === "number") console.log(`answer ${key}: ${value}`);
+// 2.
+gameEvents.delete(64);
+
+// 3.
+console.log(
+  `An event happened, on average, every ${90 / gameEvents.size} minutes`,
+);
+const time = [...gameEvents.keys()].pop();
+console.log(time);
+console.log(
+  `An event happened, on average, every ${time / gameEvents.size} minutes`,
+);
+
+// 4.
+for (const [min, event] of gameEvents) {
+  const half = min <= 45 ? "FIRST" : "SECOND";
+  console.log(`[${half} HALF] ${min}: ${event}`);
 }
-// const answer = Number(prompt("Your Answer"));
-const answer = 3;
-console.log(answer);
 
-console.log(question.get(question.get("correct") === answer));
+//Maps
 
-//map to array
-console.log([...question]);
+// const question = new Map([
+//   ["question", "what is the best prog lang in the world"],
+//   [1, "C"],
+//   [2, "Java"],
+//   [3, "JavaScript"],
+//   ["correct", 3],
+//   [true, "correct"],
+//   [false, "try again"],
+// ]);
+// console.log(question);
+
+// // object to map
+
+// console.log(Object.entries(openingHours));
+// const hoursMap = new Map(Object.entries(openingHours));
+// console.log(hoursMap);
+
+// //iterations
+
+// for (const [keys, values] of question) {
+//   if (typeof key === "number") console.log(`answer ${key}: ${value}`);
+// }
+// // const answer = Number(prompt("Your Answer"));
+// const answer = 3;
+// console.log(answer);
+
+// console.log(question.get(question.get("correct") === answer));
+
+// //map to array
+
+// console.log([...question]);
 
 // const rest = new Map();
 // rest.set("name", "classico italino");
