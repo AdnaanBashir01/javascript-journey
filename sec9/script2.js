4f258026c7a8ef296bc3245ca04201c4beec6536
AdnaanBashir01 <sheikhadnan87761@gmail.com>
2026-07-12 20:56:42 +0530

    data structures

--- sec9/script2.js
+++ sec9/script2.js
@@ -248,24 +248,24 @@ for (const flight of flights.split("+")) {
   console.log(output);
 }
 
-// practice 4
-document.body.append(document.createElement("textarea"));
-document.body.append(document.createElement("button"));
+// // practice 4
+// document.body.append(document.createElement("textarea"));
+// document.body.append(document.createElement("button"));
 
-document.querySelector("button").addEventListener("click", function () {
-  const text = document.querySelector("textarea").value;
-  const rows = text.split("\n");
+// document.querySelector("button").addEventListener("click", function () {
+//   const text = document.querySelector("textarea").value;
+//   const rows = text.split("\n");
 
-  for (const [i, row] of rows.entries()) {
-    const [first, second] = row.toLowerCase().trim().split("_");
+//   for (const [i, row] of rows.entries()) {
+//     const [first, second] = row.toLowerCase().trim().split("_");
 
-    const output = `${first}${second.replace(
-      second[0],
-      second[0].toUpperCase(),
-    )}`;
-    console.log(`${output.padEnd(20)}${"✅".repeat(i + 1)}`);
-  }
-});
+//     const output = `${first}${second.replace(
+//       second[0],
+//       second[0].toUpperCase(),
+//     )}`;
+//     console.log(`${output.padEnd(20)}${"✅".repeat(i + 1)}`);
+//   }
+// });
 
 // Practice exercise
 // const checkBaggage = function (items) {
