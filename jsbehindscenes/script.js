53a26df9e070eb83cf24b1af82e1e256f51fd7b3
AdnaanBashir01 <sheikhadnan87761@gmail.com>
2026-07-14 11:57:22 +0530

    behindscences

--- jsbehindscenes/script.js
+++ jsbehindscenes/script.js
@@ -1,27 +1,28 @@
 "use strict";
-// function caclAge(birthYear) {
-//   const age = 2037 - birthYear;
 
-//   function printAge() {
-//     const output = `you are ${age}. born in ${birthYear}`;
-//     console.log(output);
+function caclAge(birthYear) {
+  const age = 2037 - birthYear;
 
-//     if (birthYear >= 1981 && birthYear <= 1996) {
-//       var millenail = true;
-//       // creating new variable with same name as outer scope variable
-//       const firstName = "Simon";
+  function printAge() {
+    const output = `you are ${age}. born in ${birthYear}`;
+    console.log(output);
 
-//       const str = `you r millenail,${firstName}`;
-//       console.log(str);
-//     }
-//     console.log(millenail);
-//   }
-//   printAge();
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenail = true;
+      // creating new variable with same name as outer scope variable
+      const firstName = "Simon";
 
-//   return age;
-// }
-// // firstname alreagy in global scope
-// const firstName = "Bob";
-// caclAge(1994);
+      const str = `you r millenail,${firstName}`;
+      console.log(str);
+    }
+    console.log(millenail);
+  }
+  printAge();
 
-// console.log(this); //global scope
+  return age;
+}
+// firstname alreagy in global scope
+const firstName = "Bob";
+caclAge(1994);
+
+console.log(this); //global scope
